65ece6d5d1fe33eafc7fd7bca6ed5821b16f7e3d
Dylan Dislers <dylandislers@Dylans-MacBook-Pro-2.local>
2020-08-31 22:56:55 -0700

    hooking up frontend to backend register mutation

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,34 +1,63 @@
-import React from 'react'
-import { Formik, Form } from 'formik'
-import { Box, Button } from '@chakra-ui/core';
-import { Wrapper } from '../components/Wrapper';
-import { InputField } from '../components/InputField';
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
-interface registerProps {
+interface registerProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password:String! ) {
+  register(options: {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
 }
+`;
 
-export const Register: React.FC<registerProps> = ({ }) => {
+export const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant="small">
-      <Formik initialValues={{ username: "", password: "", }}
+      <Formik
+        initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
-          console.log(values)
+          return register(values);
         }}
       >
         {({ isSubmitting }) => (
           <Form>
             <InputField name="username" placeholder="username" label="User" />
             <Box mt={4}>
-              <InputField name="password" placeholder="password" label="Password" type="password" />
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
             </Box>
 
-            <Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal" > register </Button>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              {" "}
+              register{" "}
+            </Button>
           </Form>
         )}
       </Formik>
     </Wrapper>
   );
-}
+};
 
 export default Register;
