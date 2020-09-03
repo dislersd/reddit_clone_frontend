f249cbad10ebe44445dcefcf6b23cd6843709623
Dylan Dislers <dislersd@gmail.com>
2020-09-03 11:08:02 -0700

    displaying currently logged in user in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,23 +2,40 @@ import React from 'react'
 import { Box, Link, Flex } from '@chakra-ui/core';
 
 import NextLink from 'next/link'
+import { useMeQuery } from '../generated/graphql';
 
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
-  return (
-    <Flex p={4} bg="tomato">
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery()
+  let body = null;
+
+  if (fetching) {
+
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href='login'>
           <Link mr={4}>
             Login
         </Link>
         </NextLink>
-        <Link >
-          Register
+        <NextLink href='login'>
+          <Link >
+            Register
         </Link>
+        </NextLink>
+      </>
+    )
+  } else {
+    body = <Box>{data.me.username}</Box>
+  }
+  return (
+    <Flex p={4} bg="tomato">
+      <Box ml={"auto"}>
+        {body}
       </Box>
     </Flex>
   );
