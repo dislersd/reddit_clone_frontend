d7a8b56ec4104b39511258f9e7d5261a16715089
Dylan Dislers <dislersd@gmail.com>
2020-09-02 21:28:57 -0700

    login and register both working

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,28 +3,27 @@ import { Formik, Form } from "formik";
 import { Box, Button } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useRegisterMutation } from "../generated/graphql";
+import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router"
 
-interface registerProps { }
 
-export const Register: React.FC<registerProps> = ({ }) => {
+export const Login: React.FC<{}> = ({ }) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
             [{ field: 'username', message: 'something wrong' }]
-            setErrors(toErrorMap(response.data.register.errors))
-          } else if (response.data?.register.user) {
+            setErrors(toErrorMap(response.data.login.errors))
+          } else if (response.data?.login.user) {
             router.push("/");
           }
-        }} 
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
@@ -45,8 +44,7 @@ export const Register: React.FC<registerProps> = ({ }) => {
               variantColor="teal"
             >
               {" "}
-              register{" "}
-            </Button>
+              login</Button>
           </Form>
         )}
       </Formik>
@@ -54,4 +52,4 @@ export const Register: React.FC<registerProps> = ({ }) => {
   );
 };
 
-export default Register;
+export default Login;
